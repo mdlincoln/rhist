278d4c17b7714b3c086212444c7af801a5bac8d1
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-03-17 09:44:38 -0700

    set up db using triggers
    Separates package data from session-package relationships

--- R/rhist.R
+++ R/rhist.R
@@ -2,24 +2,31 @@ default_rhist_path <- function() {
   fs::path_home(".rhistory.sqlite")
 }
 
-
-install_rhist <- function() {
-  use_rhist_path()
-  initialize_rhist_db(dbpath = installed_rhist_path())
-}
-
-use_rhist_path <- usethis::use_(
-  usethis::edit_r_profile,
-  todo_text = "Specify the home for the R command history SQLite file in your .Rprofile",
-  code = c(
-    "# Set path for R history database",
-    paste0('options("rhist.path" = "', default_rhist_path(), '")')
-  ),
-  .return =
-)
-
 installed_rhist_path <- function() {
   getOption("rhist.path")
+}
+
+install_rhist <- function(dbpath = default_rhist_path()) {
+  initialize_rhist_db(dbpath)
+  use_rhist()
+}
+
+generate_rhist_last <- function(dbpath) {
+  sprintf(readLines("inst/rhist_last.R"), dbpath)
+}
+
+use_rhist <- usethis::use_(
+  usethis::edit_r_profile,
+  todo_text = "Add code to your .Rprofile that will specify the home for the R command history SQLite file, and save command history every time an interactive R sesison closes.",
+  code = generate_rhist_last(dbpath)
+)
+
+verify_rhist_db <- function(dbpath) {
+  if (is.null(dbpath))
+    stop("No path set for rhist database. Run initialize_rhist_db().")
+
+  !fs::file_exists(dbpath)
+    stop()
 }
 
 initialize_rhist_db <- function(dbpath) {
@@ -34,18 +41,68 @@ initialize_rhist_db <- function(dbpath) {
                  r_collate TEXT NOT NULL,
                  session_time INTEGER NOT NULL)")
 
-  DBI::dbExecute(db, "CREATE TABLE session_packages(
+  DBI::dbExecute(db, "CREATE TABLE package_info(
+                 pid INTEGER PRIMARY KEY,
                  package TEXT NOT NULL,
                  version TEXT NOT NULL,
                  date INTEGER NOT NULL,
                  source TEXT NOT NULL,
+                 UNIQUE (package, version, date, source))")
+
+  DBI::dbExecute(db, "CREATE TABLE session_packages(
+                 pid INTEGER NOT NULL,
                  sid INTEGER NOT NULL,
+                 FOREIGN KEY (pid) REFERENCES package_info(pid),
                  FOREIGN KEY (sid) REFERENCES session_history(sid))")
 
   DBI::dbExecute(db, "CREATE TABLE rhistory(
                  cmd NOT NULL,
                  sid INTEGER NOT NULL,
                  FOREIGN KEY (sid) REFERENCES session_history(sid))")
+
+  DBI::dbExecute(db, "CREATE TABLE session_info_holder(
+                        package TEXT NOT NULL,
+                        version TEXT NOT NULL,
+                        date INTEGER NOT NULL,
+                        source TEXT NOT NULL)")
+
+  DBI::dbExecute(db, "CREATE TABLE session_command_holder(
+                      cmd TEXT NOT NULL)")
+
+  DBI::dbExecute(db,
+"CREATE TRIGGER history_update
+AFTER INSERT ON session_command_holder
+  BEGIN
+    INSERT INTO rhistory
+    SELECT
+      cmd,
+      (SELECT MAX(sid) FROM session_history) as sid
+    FROM session_command_holder;
+
+    DELETE FROM session_command_holder;
+  END;")
+
+  DBI::dbExecute(db,
+"CREATE TRIGGER package_updater
+AFTER INSERT ON session_info_holder
+  BEGIN
+    INSERT INTO package_info
+    SELECT
+      NULL as pid,
+      package, version, date, source
+    FROM session_info_holder
+    LEFT JOIN package_info USING(package, version, date, source)
+    WHERE package_info.pid IS NULL;
+
+    INSERT INTO session_packages
+    SELECT
+      package_info.pid as pid,
+      (SELECT max(sid) from session_history) as sid
+    FROM session_info_holder
+    LEFT JOIN package_info USING(package, version, date, source);
+
+    DELETE FROM session_info_holder;
+  END;")
 
   DBI::dbDisconnect(db)
   invisible()
@@ -57,46 +114,69 @@ clear_history <- function() {
   loadhistory(tinytemp)
 }
 
-.Last <- function() {
+insert_session_info <- function(db, si) {
+  si_info <- si[["platform"]]
+  si_info <- si_info[1:5]
+  names(si_info)[5] <- "r_collate"
+  si_info$session_time <- Sys.time()
+  si_info <- as.data.frame(si_info, stringsAsFactors = FALSE)
+  DBI::dbWriteTable(db, "session_history", si_info, append = TRUE)
+}
 
-  exec_time <- system.time({
-    hist_dbpath <- "~/.rhistory.sqlite"
-    db <- DBI::dbConnect(RSQLite::SQLite(), hist_dbpath)
+insert_session_packages <- function(db, si) {
+  si_packages <- as.data.frame(si[["packages"]], stringsAsFactors = FALSE)[,c(1,3:5)]
+  si_packages[["date"]] <- as.POSIXct(si_packages[["date"]])
+
+  DBI::dbWriteTable(db, "session_info_holder", si_packages, append = TRUE)
+}
+
+collect_session_commands <- function() {
+  command_path <- tempfile()
+  savehistory(file = command_path)
+  readLines(command_path)
+}
+
+insert_session_commands <- function(db) {
+
+  session_commands <- collect_session_commands()
+
+  # If no commands were issued during the session, exit out early.
+  if (length(session_commands) == 0) return()
+
+  session_df <- data.frame(
+    cmd = session_commands,
+    stringsAsFactors = FALSE)
+
+  DBI::dbWriteTable(db, "session_command_holder", session_df, append = TRUE)
+}
+
+#' Save current session history to specified SQLite database
+#'
+#' @param dbpath Path to rhist database.
+#'
+#' @export
+save_session_history <- function(dbpath = installed_rhist_path()) {
+  try({
+    db <- DBI::dbConnect(RSQLite::SQLite(), dbpath)
+    # Enforce foreign key checks
     DBI::dbExecute(db, "PRAGMA foreign_keys = ON")
+    si <- devtools::session_info()
+
     res <- DBI::dbWithTransaction(db, {
-      si <- devtools::session_info()
-      si_info <- si[["platform"]]
-      si_info <- si_info[1:5]
-      names(si_info)[5] <- "r_collate"
-      si_info$session_time <- Sys.time()
-      si_info <- as.data.frame(si_info, stringsAsFactors = FALSE)
-      DBI::dbWriteTable(db, "session_history", si_info, append = TRUE)
-      session_id <- DBI::dbGetQuery(db, "SELECT max(sid) FROM session_history")[1,1]
-
-      si_packages <- si[["packages"]][,c(1,3:5)]
-      si_packages$sid <- rep(session_id, nrow(si_packages))
-
-      si_packages[["date"]] <- as.POSIXct(si_packages[["date"]])
-      si_packages <- as.data.frame(si_packages)
-
-      DBI::dbWriteTable(db, "session_packages", si_packages, append = TRUE)
-
-      command_path <- tempfile()
-      savehistory(file = command_path)
-      session_commands <- readLines(command_path)
-
-      session_df <- data.frame(
-        cmd = session_commands,
-        sid = session_id,
-        stringsAsFactors = FALSE
-      )
-      DBI::dbWriteTable(db, "rhistory", session_df, append = TRUE)
+      insert_session_info(db, si)
+      insert_session_packages(db, si)
+      insert_session_commands(db)
     })
-    if (res != 1) warning("Saving command history to ", hist_dbpath, " failed.")
+
+    if (!is.null(res)) {
+      if (res != TRUE) {
+        warning("Saving session and command history to ", hist_dbpath, " failed.")
+      }
+    }
+
     DBI::dbDisconnect(db)
+
     # Clear History
     clear_history()
-
-  }, gcFirst = FALSE)
-  message("Saved R history in ", exec_time[3], " seconds")
+  })
 }
